e5a9c7117213591701f7332c535a02e86f12cfe4
phich1911 <phich1911@gmail.com>
2026-06-21 00:53:45 +0700

    Add custom favicon with dark rounded background

---
+++ app/icon.tsx
@@ -0,0 +1,31 @@
+import { ImageResponse } from "next/og";
+
+export const size = { width: 32, height: 32 };
+export const contentType = "image/png";
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: 32,
+          height: 32,
+          borderRadius: 8,
+          background: "#1a1a2e",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src="https://www.abcdego.com/logo white.png"
+          width={22}
+          height={22}
+          alt=""
+        />
+      </div>
+    ),
+    { ...size }
+  );
+}
