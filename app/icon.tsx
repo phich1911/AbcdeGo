165fdbfecce91ac021d93e2e71972b429ef363e8
phich1911 <phich1911@gmail.com>
2026-06-27 14:10:27 +0700

    Fix favicon: load logo from local file with rounded corners

--- app/icon.tsx
+++ app/icon.tsx
@@ -1,14 +1,18 @@
 import { ImageResponse } from "next/og";
+import { readFile } from "fs/promises";
+import path from "path";
 
 export const size = { width: 32, height: 32 };
 export const contentType = "image/png";
 
-export default function Icon() {
+export default async function Icon() {
+  const logoData = await readFile(path.join(process.cwd(), "public", "AbcdeGo Logo.png"));
+  const base64 = `data:image/png;base64,${logoData.toString("base64")}`;
+
   return new ImageResponse(
     (
       <div style={{ display: "flex", width: 32, height: 32, borderRadius: 7, overflow: "hidden", background: "white", alignItems: "center", justifyContent: "center" }}>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src="https://www.abcdego.com/AbcdeGo Logo.png" width={24} height={24} alt="" style={{ objectFit: "contain" }} />
+        <img src={base64} width={28} height={28} style={{ objectFit: "contain" }} />
       </div>
     ),
     { ...size }
